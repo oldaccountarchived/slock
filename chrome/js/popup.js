e04bda1c98522bf5e5517309806a5831d4484533
Jacob Jenkins <jacob@jacobejenkins.com>
2016-03-30 20:59:40 -0400

    Clean up state transitions

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -1,33 +1,57 @@
+/*global chrome, $, FileReader, localStorage*/
+
 function handleKeyUpload(file, password) {
     var fileReader = new FileReader();
 
     fileReader.onload = function(event) {
         var content = event.target.result;
+
         var message = {
             messageType: "add key",
             key: content,
             password: password
         };
 
-        chrome.tabs.query({}, function(tabs) {
-            tabs.forEach(function(tab) {
-                chrome.tabs.sendMessage(tab.id, message, function(response) {
-                    console.log(response);
-                });
-            });
-        });
+        // Send key and password to open Facebook Messenger tab.
+        sendToMessengerTabs(message);
 
+        // Go to uploaded state.
         statePrivateKeyUploaded();
     };
 
     fileReader.readAsText(file);
 }
 
+function sendToMessengerTabs(message) {
+    chrome.tabs.query({}, function(tabs) {
+        tabs.forEach(function(tab) {
+            chrome.tabs.sendMessage(tab.id, message, function(response) {
+                console.log(response);
+            });
+        });
+    });
+}
+
+function doPrivateKeyUpload() {
+    var file = $('#fileInput')[0].files[0];
+    var password = $('#password').val();
+    if (password && file) {
+        handleKeyUpload(file, password);
+    } else {
+        console.log('An error occured!');
+        // Display an error
+    }
+}
+
+function doKeypairGeneration() {
+}
+
+// State transitions.
 function stateUploadOrGenerateKey() {
     $('#facebookAuth').hide();
     $('#privateKeyChoice').show();
 
-    // Add buttonlisteners.
+    // Add button listeners.
     $('#uploadKeyButton')
         .click(statePrivateKeyUpload);
     $('#genKeyButton')
@@ -48,22 +72,21 @@ function statePrivateKeyUploaded() {
     $('#privateKeyUploaded').show();
 }
 
-function doPrivateKeyUpload() {
-    var file = $('#fileInput')[0].files[0];
-    var password = $('#password').val();
-    if (password && file) {
-        handleKeyUpload(file, password);
-    } else {
-        console.log('An error occured!');
-        // Display an error
-    }
-}
-
 function stateKeypairGeneration() {
     $('#privateKeyChoice').hide();
     $('#keyPairGeneration').show();
+
+    // Add button listeners.
+    $('#doGenerateButton')
+        .click(doKeypairGeneration);
 }
 
+function stateKeypairGenerated() {
+    $('#keyPairGeneration').hide();
+    $('#keyPairGenerated').show();
+}
+
+// Set up listeners on document ready.
 function onDocumentReady() {
     if (localStorage.accessToken) {
         stateUploadOrGenerateKey();
